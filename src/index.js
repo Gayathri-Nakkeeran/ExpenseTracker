2bc35063ea6ab3b64f5058ea341ad8f393e58ea2
Gayathiri N <ngayathiri2002@gmail.com>
2023-09-29 12:04:01 +0530

    SemiColon Added

--- src/index.js
+++ src/index.js
@@ -1,12 +1,15 @@
 
 import React from 'react';
 import ReactDOM from 'react-dom';
-import App from './page/app'
+import App from './page/app';
+
 import './index.css';
 ReactDOM.render(
 
   <React.StrictMode>
 <App/>
+
+
   </React.StrictMode>,
  
     document.getElementById('root')
